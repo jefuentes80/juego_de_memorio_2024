aa6b4e7fe76e364843b8b8470e5a0e340799ab78
jefuentes <jefuentes@scl.edu.gt>
2024-02-01 13:27:01 -0600

    Repartir cartas

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// CARTAS
+let cards1 = ["🍟", "🥞", "🥯", "🥐", "🍕", "🌭", "🍞", "🍔", "🍗", "🍖", "🧀","🥫"];
+
+let cards2 = ["🍟", "🥞", "🥯", "🥐", "🍕", "🌭", "🍞", "🍔", "🍗", "🍖", "🧀", "🥫"];
+
+let todas_las_tarjetas = cards1.concat(cards2);
+
+function repartir_cartas(){
+
+    let tablero = document.querySelector("#root");
+
+    todas_las_tarjetas.forEach((cada_carta)=> {
+
+        let cart = document.createElement("div");
+        cart.innerHTML = `<div class="carta">${cada_carta}</div>`;
+
+        tablero.appendChild(cart);
+
+    })
+
+}
+
+repartir_cartas();
